3393fe3f4e9e267b26132a72bebaf4070239f7c4
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-26 01:05:10 +0530

    Welcome Emp PayRoll

---
+++ src/main/java/Employee/Demo.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Employee PayRoll");
+	}
+
+}
